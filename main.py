d6b2f0a82311eb958cde4861b41435a325aa02e8
Alexander <troshiy2011@mail.ru>
2022-09-17 17:32:51 +0300

    Path

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from schemas import *
 
 app = FastAPI()
 
+#Query = /?skip=0&limit=10
+#Path = /items/foo
 
 @app.get('/user/{pk}/items/{item}/')
 def get_user_item(pk: int, item: str):
@@ -13,10 +15,12 @@ def create_book(item : Book):
     return 
     
 @app.get('/book/')
-def get_book(q: list[str] = Query(['testbook','testbook2'], min_length=2, max_length=10, description='search Book')): #первое значение = значвение по умолчанию| сделать его обязательным = ...| regex регулярка | deprecated=True пометить параметр как устаревший| query нужно для огарничений в get запросе
+def get_book(q: list[str] = Query(['testbook','testbook2'], min_length=2, max_length=10, description='search Book')): #первое значение = значвение по умолчанию| сделать его обязательным = ...| regex регулярка | deprecated=True пометить параметр как устаревший| query нужно для параметров в get запросе
     return q
 
-
+@app.get('/book/{pk}')
+def get_book_id(pk: int = Path(..., gt=1, le=20), pages: int=Query(gt=10, le=500)):
+    return {'pk': pk, 'pages':pages}
 
 if __name__ == "__main__":
     import os
