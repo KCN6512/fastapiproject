e5ca3abdddf0bc1e9d8f18b88bd2990cfd75d7c1
Alexander <troshiy2011@mail.ru>
2023-05-21 11:48:35 +0300

    started api service

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
+import requests
 from fastapi import *
 from .schemas import *
 #docs_url=None, redoc_url=None отключить документацию docs и redoc
 app = FastAPI() 
+
 
 #Query = /?skip=0&limit=10
 #Path = /items/foo
@@ -11,43 +13,44 @@ app = FastAPI()
 # body embed = True значит добавить в json тело имя параметра
 
 @app.get('/')
-def home():
-  return {'Hello': 'world'}
+async def home():
+	return {'Hello': 'world'}
 
 @app.get('/user/{pk}/items/{item}/')
 def get_user_item(pk: int, item: str):
-    return {'user': pk, 'item': item}
+	print(requests.get('http://127.0.0.1:8000/').json())
+	return {'user_pk': pk, 'item': item}
 
 @app.post('/book/')
 def create_book(item : Book, author: Author, quantity: int = Body(..., embed=True)): #Body делает параметр передаваевым в теле а не в url
-    return {'item': item, 'author': author, 'quantity': quantity}
-    
+	return {'item': item, 'author': author, 'quantity': quantity}
+		
 @app.post('/author/')
 def create_author(author: Author):
-    '''
-    {
-  "author": {
-    "first_name": "string",
-    "last_name": "string"
-  }
+		'''
+		{
+	"author": {
+		"first_name": "string",
+		"last_name": "string"
+	}
 } с включенным Body embed True
 
 {
-  "first_name": "string",
-  "last_name": "string"
+	"first_name": "string",
+	"last_name": "string"
 } без embed
-    '''
-    return {'author': author}
+		'''
+		return {'author': author}
 
 @app.get('/book/')
 def get_book(q: list[str] = Query(['testbook','testbook2'], min_length=2, max_length=10, description='search Book')): #первое значение = значвение по умолчанию| сделать его обязательным = ...| regex регулярка | deprecated=True пометить параметр как устаревший| query нужно для параметров в get запросе
-    return q
+	return q
 
 @app.get('/book/{pk}')
 def get_book_id(pk: int = Path(..., gt=1, le=20), pages: int=Query(gt=10, le=500)):
-    return {'pk': pk, 'pages':pages}
+	return {'pk': pk, 'pages':pages}
 
 if __name__ == "__main__":
-    import os
-    command = 'uvicorn app.main:app --reload'
-    os.system(command)    
+	import os
+	command = 'uvicorn app.main:app --reload'
+	os.system(command)    
--- app/test_main.py
+++ app/test_main.py
@@ -1,12 +1,13 @@
 from starlette.testclient import TestClient
 
-from main import app
+from .main import app
 
 client = TestClient(app)
 
 
 def test_read_item():
     response = client.get("/items/foo", headers={"X-Token": "coneofsilence"})
+    assert response.headers == None
     assert response.status_code == 200
     assert response.json() == {
         "id": "foo",
