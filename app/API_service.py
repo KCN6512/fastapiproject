df8d7a2ef1516b9ad5f1c84f4935489ac1d10918
Alexander <troshiy2011@mail.ru>
2023-05-24 13:31:52 +0300

    path parameters

--- app/API_service.py
+++ app/API_service.py
@@ -1,3 +1,6 @@
+from enum import Enum
+import json
+import os
 from typing import Optional, Any
 import psycopg2
 from fastapi import FastAPI, Query
@@ -23,7 +26,7 @@ app = FastAPI()
 
 # @app.get('/')
 # def home(params: list[int] | None = Query(lt=10, default=None), **other: Any | None):
-#     print(other.items())
+#     print(other.products())
 #     return {'text': 'home_page'}, params, other
 
 # If your application (somehow) doesn't have to communicate with anything else and wait for it to respond, use async def.
@@ -32,7 +35,28 @@ app = FastAPI()
 async def home():
 	return {'text': 'home_page'}
 
-# if __name__ == "__main__":
-# 	import os
-# 	command = 'uvicorn app.API_service:app --reload'
-# 	os.system(command)    
+@app.get('/product/{product}')
+async def product(product: str):
+	return {'product': product}
+
+class ProductName(str, Enum):
+    smartphone = "smartphone"
+    tv = "tv"
+    cpu = "cpu"
+
+@app.get("/products/{product_name}")
+async def get_product(product_name: ProductName):
+    if product_name is ProductName.smartphone:
+        return {"product_name": product_name, "message": "smartphone"}
+
+    if product_name.value == "tv":
+        return {"product_name": product_name, "message": "tv"}
+
+    return {"product_name": product_name, "message": "cpu"}
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    print(file_path)
+    print(os.path.exists(file_path))
+    return {"file_path": file_path}
+
