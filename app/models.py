fa3c54d0b0163da13803543dd83e56d469b5d9a8
Alexander <troshiy2011@mail.ru>
2023-06-04 19:02:54 +0300

    Revert "minor"
    This reverts commit 379cb689507df08b1b442354cb6203c4cb6fb068.

--- app/models.py
+++ app/models.py
@@ -9,4 +9,4 @@ class Product(BaseModel):
     name: str = Field(title='item name',default='default_name', max_length=100)
     description: str | None = 'default_description' # non required field with default value
     price: float = Field(gt=1.0)
-    category: Category # set[Category] = set() non
+    category: Category
